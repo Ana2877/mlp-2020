8aa96c0c4654198d800b74972c777b9bc750e52b
Ana Carolina Pagnoncelli <acpagnoncelli@inf.ufrgs.br>
2020-11-01 19:30:38 -0300

    tarefa 2

--- respostas/exe2/exec.py
+++ respostas/exe2/exec.py
@@ -15,15 +15,22 @@ def exercicio_3(df):
     print(df.groupby('Qualis').count()[['Tipo']])
 
 def exercicio_4(df):
-    print(df.groupby(['Ano', 'Qualis']).count()[['Tipo']])
+    prodPPGCByTipoporAno = df.groupby(['Ano','Qualis'])
+    print(prodPPGCByTipoporAno.size().unstack())
+    prodPPGCByTipoporAno.size().unstack().plot()
+    plt.show()
+
+def exercicio_5(df):
+    print(df['Aut.Total'].mean())
 
 
 def menu():
     menu =  "\n\n0 - Sair\n"+\
             "1 - Número de artigos produzidos por ano\n"+\
-            "2 - Número de artigos publicados por ano por tipo e gráfico\n"+\
+            "2 - Número de artigos publicados por ano e por tipo e gráfico\n"+\
             "3 - Número de artigos por categorias\n"+\
-            "4 - Número de artigos por categoria e ano"
+            "4 - Número de artigos por categoria e por ano e gráfico\n"+\
+            "5 - Número médio de autores por artigo\n\n"
     print(menu)
 
 
@@ -36,14 +43,8 @@ def main():
     while(numero != 0):
         menu()
         numero = int(input())
-        if numero == 1:
-            exercicio_1(df)
-        if numero == 2:
-            exercicio_2(df)
-        if numero == 3:
-            exercicio_3(df)
-        if numero == 4:
-            exercicio_4(df)
+        if numero>0 and numero<6:
+            globals()["exercicio_" + str(numero)](df)
 
 if (__name__ == "__main__"):   
       main()                  
